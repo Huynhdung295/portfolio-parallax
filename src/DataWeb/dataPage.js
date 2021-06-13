382804655b7107b77a2236a6f7e1ccdc514ac01c
Huỳnh Dũng <@huynhdungfed>
2021-06-13 18:06:24 +0700

    a

--- src/DataWeb/dataPage.js
+++ src/DataWeb/dataPage.js
@@ -2,22 +2,24 @@ import Home from "../Component/Home/Home";
 import About from "../Component/About/About";
 import Contact from "../Component/Contact/Contact";
 
+
+
 const dataPage = [
   {
     page: "Home",
-    url: "https://source.unsplash.com/Im7lZjxeLhg/1600x900",
+    url: "https://source.unsplash.com/Im7lZjxeLhg" ,
     id: "home",
     content: <Home />,
   },
   {
     page: "About",
-    url: "https://source.unsplash.com/cckf4TsHAuw/1600x900",
+    url: "https://source.unsplash.com/cckf4TsHAuw",
     id: "about",
     content: <About />,
   },
   {
     page: "Portfolio",
-    url: "https://source.unsplash.com/aOC7TSLb1o8/1600x900",
+    url: "https://source.unsplash.com/aOC7TSLb1o8",
     id: "portfolio",
     content: "",
   },
